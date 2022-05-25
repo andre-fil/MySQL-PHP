c47611e4acba6d45bcdffbaa7af85b7379be6f46
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-05-25 13:14:23 -0300

    primeira parte php e html

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="modelo/css.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listagem de Livros</title>
+</head>
+<body>
+    <div id="corpo">
+        <h1>Escolha seu livro</h1>
+        <table class="listagem">
+            <tr><td>Foto<td>Nome<td>Info
+            <tr><td>Foto<td>Nome<td>Info
+            <tr><td>Foto<td>Nome<td>Info
+            <tr><td>Foto<td>Nome<td>Info
+        </table>
+    </div>
+</body>
+</html>
