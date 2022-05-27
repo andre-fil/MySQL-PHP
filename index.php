5e5e526a782ef01d78cd09f335c15731a0d43a19
André Filipe Sousa  Barreto <barreto.andre@discente.ufma.br>
2022-05-27 17:31:57 -0300

    imagens

--- index.php
+++ index.php
@@ -2,12 +2,26 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="modelo/css.css">
+    <link rel="stylesheet" href="estilos/css.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listagem de Livros</title>
+    <?php
+    function thumb ($arq){
+        $caminho = "fotos/$arq";
+        if (is_null($arq) || !file_exists($caminho)){
+            return "fotos/indis.png";
+        } else{
+            return $caminho;
+        }
+    }
+
+    ?>
+
 </head>
 <body>
+   
+    
     <?php
         #include "includes/banco.php"
         require_once "includes/banco.php"
@@ -16,16 +30,25 @@
         <h1>Escolha seu livro</h1>
         <table class="listagem">
             <?php
+                #definindo um objeto de busca - pegando todos os registros da tabela livros
                 $busca = $banco->query("select * from livros order by nome");
 
+                #verificando se a busca foi bem sucedida//se retornou algum registro
                 if(!$busca){
                     echo"<tr><td>Foi encontrado um erro na busca!</td></tr>";
                 }else{
+
+                    #num_rows = atributo que define quantos resultados foram retornados
                     if($busca->num_rows == 0){
                         echo "<tr><td>Não foi retornado nenhum registro!</td></tr>";
                     } else{
-                        while($reg = $busca->fetch_object())
-                        echo "<tr><td>$reg->capa</td><td>$reg->nome</td></tr>";
+
+                        #pegando o resultado das buscas na var reg e mostrando cada campo
+                        while($reg = $busca->fetch_object()){
+                            $t = thumb($reg->capa);
+                            echo"<tr><td><img src='$t' class='mini'/> <td>$reg->nome";
+                            echo"<td>info";
+                        }
                     }
                 }
             ?>
@@ -34,3 +57,5 @@
     </div>
 </body>
 </html>
+
+<img src="" alt="" srcset="">
